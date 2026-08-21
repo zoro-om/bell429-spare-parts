ee7f2087f58ffc109c6581346281593ba87657c7
ZORO <262745013+zoro-om@users.noreply.github.com>
2026-08-22 00:36:11 +0400

    Delete functions directory

--- functions/api/auth/login.js
+++
@@ -1,2 +0,0 @@
-import { login } from "../../_lib/auth.js";
-export async function onRequestPost(context) { return login(context.request, context.env); }
--- functions/api/auth/me.js
+++
@@ -1,2 +0,0 @@
-import { me } from "../../_lib/auth.js";
-export async function onRequestGet(context) { return me(context.request, context.env); }
